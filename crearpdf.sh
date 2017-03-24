561c271274c4c89ed6809d38f5cef893b2c6f3b6
Luis Octavio Ramírez Fernández <lramirez88mx@gmail.com>
2017-03-24 00:46:46 -0600

    Ajustes de diseño en numeración, APA en tablas e imágenes

--- crearpdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script para crear el archivo PDF de la tarea
-# Modifica las variable TAREA Y TAREAPDF de acuerdo a tus archivos
-TAREA=PaternoMaternoN_tarea.md
-TAREAPDF=PaternoMaternoN_tarea.pdf
-pandoc -s -S --template plantilla_tarea.tex --filter pandoc-citeproc --csl estilo/apa.csl --bibliography biblio/bibliografia.bib -o $TAREAPDF $TAREA
